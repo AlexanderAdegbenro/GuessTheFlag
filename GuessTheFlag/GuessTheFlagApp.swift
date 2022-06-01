43b192194c6a71d802c26eb7df8a005fb7449f9d
Alexander Adegbenro <52863002+AlexanderAdegbenro@users.noreply.github.com>
2022-06-01 18:38:23 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alexander Adgebenro on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
